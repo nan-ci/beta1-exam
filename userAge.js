8af8cc67f16b310eacf9b75d1f4b6b2cfccd71c4
Yoann Cribier <ycribier@student.42.fr>
2017-12-10 11:54:53 +0000

    Create userAge.js

---
+++ userAge.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/*
+ * Create a function `userAge` which take an object user and returns its age
+ *
+ * Créer une fonction `userAge` qui prend un object user et qui retourne son age
+ *
+ */
+
+// Your code :
