c8367a6d521d644740c921ed388d7aec88f156fa
Yoann Cribier <ycribier@student.42.fr>
2017-12-10 11:49:27 +0000

    Create user.js

---
+++ user.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*
+ * Create an object `user` with properties:
+ *  'name' (String),
+ *  'email' (String),
+ *  'age' (Number),
+ * 
+ * Properties should have valid values (you can use your personnal infos)
+ *
+ *
+ * Créer un object `user` avec les propriétes:
+ *  'name' (String),
+ *  'email' (String),
+ *  'age' (Number)
+ *
+ * Les propriétes doivent avoir des valeurs valides (vous pouvez utiliser vos
+ * infos personnelles)
+ * 
+ */
+
+// Your code :
