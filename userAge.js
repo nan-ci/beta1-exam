29999d8230c5b3e68e833159fe9872485b65590d
Yoann Cribier <ycribier@student.42.fr>
2017-12-10 14:59:44 +0000

    Update userAge.js

--- userAge.js
+++ userAge.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /*
- * Create a function `userAge` which take an user object and returns its age
+ * Create a function `userAge` which take a user object and returns its age
  *
  * Créer une fonction `userAge` qui prend un object user et qui retourne son age
  *
